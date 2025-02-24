a6804c9574a2177944777602e35e85079a3c563a
suryanra <suryapratapnra@gmail.com>
2025-02-25 00:08:27 +0530

    addding the search functionality

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import './Booking.css';
 import movies from "./movie";
@@ -6,16 +6,25 @@ import NavBar from "./NavBar";
 import MovieCard from "./MovieCard";
 
 const Booking = () => {
-  console.log(movies);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const filteredMovies = movies.filter(movie =>
+    movie.name.toLowerCase().includes(searchQuery)
+  );
+
   return (
     <>
-      <NavBar />
-      <span className='greet'>Good Morning Mr.Naval Ravikant !</span>
+      <NavBar setSearchQuery={setSearchQuery} />
+      <span className='greet'>Good Morning Mr. Naval Ravikant!</span>
       <div className="movie-container">
-        {movies.map((movie)=>(<MovieCard data={movie}/>))
-        }
+        {filteredMovies.length > 0 ? (
+          filteredMovies.map((movie) => <MovieCard key={movie.id} data={movie} />)
+        ) : (
+          <p>No movies found</p>
+        )}
       </div>
     </>
   );
 };
+
 export default Booking;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,28 @@
-import React from 'react'
+import React from 'react';
 import { IoMdSearch } from "react-icons/io";
 import { FaRegUserCircle } from "react-icons/fa";
 import { useContext } from "react";
 import { AuthContext } from "../context/authContext";
-const NavBar = () => {
-        const { user, setUser } = useContext(AuthContext);
-  return (
-      <div className="navbar-container">
-        <div className="search-bar">
-          <input type="text" placeholder="Search..." />
-          <IoMdSearch className="search-icon" />
-          <br/>
-        </div>
-        
-        <div className="user-name">
-          <FaRegUserCircle /> {user}
-        </div>
-      </div>
-  )
-}
 
-export default NavBar
+const NavBar = ({ setSearchQuery }) => {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <div className="navbar-container">
+      <div className="search-bar">
+        <input 
+          type="text" 
+          placeholder="Search..." 
+          onChange={(e) => setSearchQuery(e.target.value.toLowerCase())} 
+        />
+        <IoMdSearch className="search-icon" />
+      </div>
+      
+      <div className="user-name">
+        <FaRegUserCircle /> {user}
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
